55c9f8c322487733b40da0aa5a886f36d57fbb0f
João Pedro Barbosa da Silva <jpedro0807@hotmail.com>
2025-06-05 21:38:59 -0300

    Activity Home

--- app/src/main/java/com/example/risk_free_admin/HomeActivity.kt
+++ app/src/main/java/com/example/risk_free_admin/HomeActivity.kt
@@ -1,6 +1,9 @@
 package com.example.risk_free_admin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,5 +12,25 @@ import androidx.core.view.WindowInsetsCompat
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)}
+        setContentView(R.layout.activity_home)
+        val btnVerificarAmeacas: Button = findViewById(R.id.btnVerificarAmeacas)
+
+        val btnRelatorios: Button = findViewById(R.id.btnRelatorios)
+
+        val btnNavVerificarAmeacas : ImageButton = findViewById(R.id.btnNavVerificarAmeacas)
+
+        val btnNavExportRelatorio : ImageButton = findViewById((R.id.btnNavExportRelatorio))
+
+        val navegarPraReport = {
+            startActivity(Intent(this, VerificarAmeacaActivity::class.java))
+        }
+        val navegarPraRelatoriosActivity = {
+            startActivity(Intent(this, RelatoriosActivity::class.java))
+        }
+
+        btnVerificarAmeacas.setOnClickListener { navegarPraReport() }
+        btnNavVerificarAmeacas   .setOnClickListener { navegarPraReport() }
+        btnRelatorios.setOnClickListener{navegarPraRelatoriosActivity()}
+        btnNavExportRelatorio.setOnClickListener{navegarPraRelatoriosActivity()}
+    }
 }
